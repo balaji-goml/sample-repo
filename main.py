df8a89be7105555a1881ac1d54673562386268fe
Balaji <balaji.b@goml.io>
2025-09-12 12:06:02 +0530

    fixed

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from add import add
-from subtract import subtract
-from multiply import multiply
 from divide import divide
+from multiply import multiply
+from subtract import subtract
+
 
 def main():
     a = 10
     b = 0
- 
+
     print(f"{a} + {b} = {add(a, b)}")
     print(f"{a} - {b} = {subtract(a, b)}")
     print(f"{a} * {b} = {multiply(a, b)}")
     print(f"{a} / {b} = {divide(a, b)}")
-    
+
+
 if __name__ == "__main__":
     main()
